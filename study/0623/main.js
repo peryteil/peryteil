85593ab593a526a58e88e18e17fb5573d62103c1
peryteil <peryteil@gmail.com>
2025-06-23 12:52:42 +0900

    0623 javascript 실습

---
+++ study/0623/main.js
@@ -0,0 +1,16 @@
+// alert("Hellow World")
+// console.log("Hello World");
+// document.write("<h1>Document Write</h1>")
+
+// let currentYear = 2025;
+// let birthYear = prompt("태어난 해를 입력하세요.")
+// let age = currentYear - birthYear + 1;
+// alert("당신의 나이는 " + age + "살 입니다.")
+
+let name = "철수";
+let age = 18;
+let isStudent = true;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isStudent); // boolean
