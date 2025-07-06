c0274e36977889e329b0c047e29e9ddd8c8e8474
peryteil <peryteil@gmail.com>
2025-07-06 15:52:48 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,5 @@
+function solution(num_list) {
+    const product = num_list.reduce((acc, cur) => acc * cur, 1);
+    const sum = num_list.reduce((acc, cur) => acc + cur, 0);
+    return product < sum ** 2 ? 1 : 0;
+}
