ea3bf2b1e2cdcd491ee35af8a0c37f587a7bd76a
peryteil <peryteil@gmail.com>
2025-07-16 10:20:22 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,5 +1,9 @@
-function solution(n) {
-    const lastEven = n % 2 === 0 ? n : n - 1;
-    const count = lastEven / 2;
-    return count * (2 + lastEven) / 2;
+function solution(n){
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        if( i % 2 === 0){
+            sum += i;
+        }
+    }
+    return sum;
 }
