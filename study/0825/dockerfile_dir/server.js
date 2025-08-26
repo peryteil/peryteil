746c7e61de8083d003aa9c4d8c8b9acc5e874199
john <test@google.com>
2025-08-26 11:20:46 +0900

    cloud

---
+++ study/0825/dockerfile_dir/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 기본 라우트
+app.get('/', (req, res) => {
+  res.send('Hello, Docker + Node.js!');
+});
+
+// 서버 실행
+app.listen(port, () => {
+  console.log(`��� Server running at http://localhost:${port}`);
+});
